2cfede1946ba1be97518496fd6873250e4f51465
GarimamodiIIT <csz188010@iitd.ac.in>
2020-04-29 15:22:27 +0530

    Message

--- Gitpush.sh
+++ Gitpush.sh
@@ -3,5 +3,5 @@ git status -s
 git add .
 git status -s
 git commit -m "Message"
-git remote add origin git@github.com:GarimamodiIIT/first_repo
+git remote add origin1 git@github.com:GarimamodiIIT/first_repo
 git push -u origin master
